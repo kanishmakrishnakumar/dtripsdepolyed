0db9a62e527e0e25230b6bd123d74f71e0ab83ef
kanishmakrishnakumar <115967330+kanishmakrishnakumar@users.noreply.github.com>
2022-11-29 12:39:57 +0530

    Update HotelBook.vue

--- src/components/hotel/HotelBook.vue
+++ src/components/hotel/HotelBook.vue
@@ -15,6 +15,14 @@
         <br>
     <v-form @submit.prevent>
     <v-card class="ml-6 mr-6" flat>
+      <!-- <div>
+    <input v-model="searchTerm" type="text">
+    <div></div>
+    <div v-for="place in filterByTerm" :key="place.id">
+      <span>{{ place.country }}</span>
+    </div>
+  </div> -->
+      <!-- <v-btn @click="SelectDestination">select</v-btn> -->
       <v-row>
             <v-col
           cols="12"
@@ -22,15 +30,57 @@
           md="2" 
           dense
         >
-        <v-select color="purple"
+        <!-- <v-text-field color="purple"
+            label="Select destination"
+            outlined
+            v-model="searchTerm"
+            single-line
+            hide-details 
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field> -->
+        <section class="dropdown-wrapper">
+          <div @click="isVisible = !isVisible" class="selected-item">
+            <span v-if="selectedItem">{{destPlace}}, {{selectedItem}}</span>
+            <span v-else>Select Destination</span>
+          </div> 
+          <div v-if="isVisible" class="dropdown-popover">
+            <input v-model="search" type="text" placeholder="Search Destination" @input="SelectDestination">
+            <span v-if="filterDestination.length === 0">No result found</span>
+            <div class="options">
+              <ul>
+                <li @click="selectItem(place)" v-for="(place, index) in filterDestination" :key="`place-${index}`"> {{place.destination}},{{ place.country }}</li>
+              </ul>
+            </div>
+          </div> 
+        </section>
+        <!-- <v-select color="purple"
             :items="hotels"
             label="Select destination"
             outlined
-            ></v-select>
-          <!-- <v-text-field color="purple"
+            ></v-select> -->
+            <!-- <v-menu
+            v-model="select"
+            ref="menu" offset-y :close-on-content-click="false"
+          >
+          <template v-slot:activator="{ on, attrs }">
+          <v-text-field color="purple"
             label="Select destination"
             outlined
-          ></v-text-field> -->
+            v-model="searchTerm"
+            single-line
+            hide-details 
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <div v-if="filterByTerm.length">
+        <v-list v-for="place in filterByTerm" :key="place.id">
+          <span>{{ place.country }}</span>
+        </v-list>
+        </div>
+        <div v-else>No result found</div>
+            </v-menu> -->
           </v-col>
           <v-col
             cols="12"
@@ -115,7 +165,7 @@
                 <v-card-title class="text-h5">
                 Rooms &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <v-btn class="control-button" @click="decrease">-</v-btn>
-                &nbsp; {{ Room }} &nbsp;
+                &nbsp; {{ Room }}
                 <v-btn class="control-button" @click="increase">+</v-btn>
               </v-card-title><v-divider></v-divider>
             </div> 
@@ -125,14 +175,14 @@
               <v-card-title class="text-h5">
                 Adults<h6 class="text-center grey--text">(+18yrs)</h6> 
                 <v-btn class="control-button" @click="decreaseAdult(index)">-</v-btn>
-                &nbsp; {{ key.NoOfAdults }} &nbsp;
+                &nbsp; {{ key.NoOfAdults }}
                 <v-btn class="control-button" @click="increaseAdult(index)">+</v-btn>
               </v-card-title>
                            
               <v-card-title class="text-h5">
                 Child <h6 class="text-center grey--text">&nbsp;(&lt;18yrs)</h6> &nbsp; 
                 <v-btn class="control-button" @click="decreaseChild(index)">-</v-btn>
-                &nbsp; {{ key.NoOfChild }} &nbsp;
+                &nbsp; {{ key.NoOfChild }} 
                 <v-btn class="control-button" @click="increaseChild(index)" >+</v-btn>
 
         <div v-for="(age,index) in key.ChildAge" :key="index" :load="log(age)">
@@ -184,8 +234,10 @@ import axios from 'axios'
   export default {
     data:()=>({
       tab: null,
+      search: '',
       // isLoggingIn: false,
       dialog: false,
+      select: false,
       date1: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
       // date2: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
       date2: '',
@@ -201,11 +253,24 @@ import axios from 'axios'
       dropdowns: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17],
       hotels:[],
       diff:'',
-      clientIp:''
+      clientIp:'',
+      search:'',
+      selectedItem: null,
+      isVisible: false,
+      destination:[],
+      destPlace:'',
+      cityId:'',
+      countrycode:''
       
     }),
 
     computed: {
+        filterDestination(){
+            return this.destination.filter(place => {
+              return place.country.toLowerCase().includes(this.search.trim().toLowerCase()) || 
+                place.destination.toLowerCase().includes(this.search.trim().toLowerCase());
+        });
+      } ,
         doubleValue: {
             get(){
               console.log(this.date1),
@@ -223,8 +288,6 @@ import axios from 'axios'
               this.items.map(item => item.NoOfChild)
               .reduce((prev, current) =>this.Childern = prev + parseInt(current,10), 0);
 
-
-              
               // this.items.forEach(itemsArry => {
               //   // this.Adults += parseInt(itemsArry.Adult);
               //   this.Adults = itemsArry.Adult + itemsArry.Child
@@ -240,11 +303,39 @@ import axios from 'axios'
       },
       
     mounted() {
-          // this.getHoteldetails();
           this.getIpAddress ();
+          this.SelectDestination();
         },
 
       methods:{
+        selectItem(place){
+          this.selectedItem = place.country ;
+          this.destPlace = place.destination;
+          this.countrycode = place.countrycode;
+          console.log(this.countrycode);
+          this.cityId = place.cityid;
+          console.log(this.cityId);
+          this.isVisible = false;
+        },
+        SelectDestination(search){
+          console.log('response.status');
+          console.log(search);
+          axios.post("http://192.168.1.40:8991/api/city/page",{
+              "off": 0,
+              "on" :7,
+              "keyword": this.search
+          })
+          .then((response)=>{
+                console.log(response.status);
+                console.log(response.data);
+                this.dest = response.data;
+                this.destination = [...new Set(this.dest)]
+                console.log("this.destination")
+                console.log(this.destination)
+          }).catch((error)=>{
+                console.log(error.response);
+          });
+      },
         getHoteldetails(){
           console.log(this.date1);
            const Night = localStorage.getItem('DateDiff');
@@ -254,7 +345,7 @@ import axios from 'axios'
           console.log(this.clientIp);
           const IPAddress = localStorage.getItem('IPAddress');
           console.log(IPAddress)
-              axios.post('https://dtrips.herokuapp.com/api/hotels/search',{
+              axios.post('http://192.168.1.40:8991/api/hotels/search',{
                     "CheckInDate": this.date1,
                     "NoOfNights": this.diff,
                     "ResultCount": 0,
@@ -265,10 +356,10 @@ import axios from 'axios'
                     "NoOfRooms": this.Room,
                     "IsNearBySearchAllowed": false,
                     "RoomGuests": this.items,
-                    "CityId": 130443,
+                    "CityId": this.cityId,
                     "MinRating": 1,
                     "TokenId": "c8035888-abcc-4897-9206-ce71439e3ae0",
-                    "CountryCode": "IN",
+                    "CountryCode": this.countrycode,
                     "ReviewScore": 0,
                     "EndUserIp": IPAddress
                 })
@@ -354,11 +445,6 @@ import axios from 'axios'
             console.log(this.Room++)
               this.items.push({NoOfAdults: 1, NoOfChild: 0, ChildAge: []});
           },
-    
-          // changeFor(index){
-          //   this.items[index] = { Adult: '', Child: '' };
-          //   console.log(`index ${index} changed to ${JSON.stringify(this.items[index], null, '\t')}`);
-          // },/
 
            getIpAddress () {
             fetch('https://api.ipify.org?format=json')
@@ -372,3 +458,68 @@ import axios from 'axios'
       }
   }
 </script>
+
+<style scoped lang="scss">
+.dropdown-wrapper{
+  max-width: 350px;
+  position: relative;
+  margin: 0 auto;
+}
+.selected-item{
+  height: 57px;
+  border: 2px solid lightgray;
+  border-radius: 5px;
+  padding: 5px 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 18px;
+  font-weight: 400;
+}
+.dropdown-popover{
+  position: absolute;
+  border: 2px solid lightgrey;
+  top: 46px;
+  left :0;
+  right: 0;
+  background-color: #fff;
+  max-width: 100%;
+  padding: 10px;
+
+  input{
+    width: 100%;
+    height: 50px;
+    border: 2px solid lightgray;
+    font-size: 16px;
+    padding-left: 8px;
+  }
+  .options{
+    width: 100%;
+
+    ul{
+      list-style: none;
+      text-align: left;
+      padding-left: 8px;
+      max-height: 200px;
+      overflow-y: scroll;
+      overflow-x: hidden;
+      border: 1px solid lightgray;
+
+      li{
+        width: 100%;
+        border-bottom: 1px solid lightgray;
+        padding: 10px;
+        background-color: #f1f1f1;
+        cursor: pointer;
+        font-size: 16px;
+        &:hover {
+          background: #70878a;
+          color: #fff;
+          font-weight: bold;
+        }
+      }
+    }
+  }
+}
+
+</style>
